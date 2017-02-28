817c48973215d33294a4e901dff6427db529ac0d
faizalZebua <faizal.ahmad27@icloud.com>
2017-02-28 18:05:50 +0700

    Pertemuan 2 teori NimNamaKelas

---
+++ Pertemuan2/NimNamaAlamat/Mahasiswa.java
@@ -0,0 +1,7 @@
+public class Mahasiswa {
+    public static void main(String[] args){
+       System.out.println("NIM    : 10115548");
+       System.out.println("Nama   : Dwi Ahmad Faizal Zebua");
+       System.out.println("Alamat : Jln. Semboja Ds. Damarguna Kec. Ciledug Kab. Cirebon");
+    }
+}
